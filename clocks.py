7a91fd6c0175d7f87f5a9e7f31d4a54bb07c309e
Joshua Charles Campbell <gitcommit@orezpraw.com>
2014-01-05 14:21:45 -0700

    nothing

--- clocks.py
+++ clocks.py
@@ -7,11 +7,11 @@ import pprint
 import api
 import sys
 
-mem_min = 1250
+mem_min = 1000
 mem_max = 1500
 mem_step = 5
-core_min = 800
-core_max = 890
+core_min = 700
+core_max = 1200
 core_step = 5
 
 existing = open('first.csv', 'r')
@@ -23,7 +23,7 @@ for mem, core, mhz in [_.split(',') for _ in existing]:
 for mem, core in product(range(mem_min, mem_max, mem_step), range(core_min, core_max, core_step)):
     if not '%i.%i' % (mem, core) in skip:
         os.system('aticonfig --adapter=all --odsc=%i,%i >/dev/null' % (core,mem))
-        time.sleep(15)
-        mhs = api.devs()[1]['MHS 5s']
+        time.sleep(20)
+        mhs = api.devs()[0]['MHS 5s']
         print '%i,%i,%f' % (mem, core, mhs)
         sys.stdout.flush()
