0fccc7908728286707e280c91f0d67cdb7729b17
vitaminmoo <vitaminmoo@wza.us>
2014-01-04 20:06:07 -0800

    Various fixes
    - make card configurable
    - actually reuse the file

--- clocks.py
+++ clocks.py
@@ -3,27 +3,32 @@
 import time
 import os
 from itertools import product
-import pprint
 import api
 import sys
+
+csv='mhs.csv'
+card = 1
 
 mem_min = 1250
 mem_max = 1500
 mem_step = 5
-core_min = 800
-core_max = 890
+core_min = 890
+core_max = 895
 core_step = 5
 
-existing = open('first.csv', 'r')
-
 skip = []
-for mem, core, mhz in [_.split(',') for _ in existing]:
-    skip.append('%s.%s' % (mem,core))
+try:
+    with open(csv, 'r') as output:
+        for mem, core, mhz in [_.split(',') for _ in output]:
+            skip.append('%s,%s' % (mem,core))
+except IOError:
+   pass
 
-for mem, core in product(range(mem_min, mem_max, mem_step), range(core_min, core_max, core_step)):
-    if not '%i.%i' % (mem, core) in skip:
-        os.system('aticonfig --adapter=all --odsc=%i,%i >/dev/null' % (core,mem))
-        time.sleep(15)
-        mhs = api.devs()[1]['MHS 5s']
-        print '%i,%i,%f' % (mem, core, mhs)
-        sys.stdout.flush()
+with open(csv, 'a') as output:
+    for mem, core in product(range(mem_min, mem_max, mem_step), range(core_min, core_max, core_step)):
+        if not '%i,%i' % (mem, core) in skip:
+            os.system('aticonfig --adapter=all --odsc=%i,%i >/dev/null' % (core,mem))
+            time.sleep(15)
+            mhs = api.devs()[card]['MHS 5s']
+            output.write('%i,%i,%f\n' % (mem, core, mhs))
+            sys.stdout.flush()
