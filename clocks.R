ae13c5f9c714aa3272f0c3610a51f3c2194bc56f
Joshua Charles Campbell <gitcommit@orezpraw.com>
2014-01-05 19:02:35 -0700

    fix gradient

--- clocks.R
+++ clocks.R
@@ -1,4 +1,5 @@
 library(ggplot2)
 infile <- read.csv(file="mhs.csv", header=TRUE,sep=",")
-out <- ggplot(data=infile, aes(x=mem, y=core, fill=mhs)) + geom_tile() + scale_fill_hue()
+out <- ggplot(data=infile, aes(x=mem, y=core, fill=mhs)) + geom_tile() + scale_colour_gradientn(colours = rainbow(7))
+
 ggsave(out,filename='clocks.png')
