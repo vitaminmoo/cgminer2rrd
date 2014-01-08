4a452be2a9e5778761ab5bf988455c160e08d5c9
vitaminmoo <vitaminmoo@wza.us>
2014-01-07 22:12:50 -0800

    Misc cleanup

--- clocks.py
+++ clocks.py
@@ -1,29 +1,31 @@
 #!/usr/bin/env python
 
-import time
-import os
 from itertools import product
 import api
+import numpy as np
+import os
+import scipy as sp
+import scipy.stats
 import sys
+import time
 
 csv='mhs.csv'
 card = 0
 
 mem_min = 1250
 mem_max = 1460
-mem_step = 12
+mem_step = 3
 core_min = 830
 core_max = 890
-core_step = 12
-desired_accuracy_in_mhs = 0.001
+core_step = 1
+desired_accuracy_in_mhs = 0.002
 infinity = 1.0e24
 
-import numpy as np
-import scipy as sp
-import scipy.stats
+clock_command = 'aticonfig --adapter=all --odsc=%i,%i >/dev/null'
+
 
 def mean_confidence(data, confidence=0.95):
-    a = 1.0*np.array(data)
+    a = 1.0 * np.array(data)
     n = len(a)
     if n == 0:
       print "not enough data"
@@ -32,8 +34,6 @@ def mean_confidence(data, confidence=0.95):
     h = se * sp.stats.t._ppf((1+confidence)/2., n-1)
     print "h: " + str(h)
     return h
-
-
 
 skip = []
 try:
@@ -47,9 +47,14 @@ except IOError:
     pass
 
 with open(csv, 'a') as output:
-    for mem, core in product(range(mem_min, mem_max+1, mem_step), range(core_min, core_max+1, core_step)):
+    for mem, core in product(
+        xrange(mem_min, mem_max+1, mem_step),
+        xrange(core_min, core_max+1, core_step)
+    ):
         if not (mem, core) in skip:
-            os.system('aticonfig --adapter=all --odsc=%i,%i >/dev/null' % (core,mem))
+            os.system(clock_command % (core,mem))
+            print 'adjusting clock to %i,%i' % (mem, core)
+
             samples = []
             time.sleep(10) # wait 15s for first sample, 5s else
             while len(samples) < 3 or mean_confidence(samples) > desired_accuracy_in_mhs:
@@ -57,7 +62,9 @@ with open(csv, 'a') as output:
               sample = api.devs()[card]['MHS 5s']
               samples.extend([sample])
             mhs = np.mean(np.array(samples))
+
             output.write('%i,%i,%f\n' % (mem, core, mhs))
             output.flush()
+
             print '%i,%i,%f' % (mem, core, mhs)
             sys.stdout.flush()
